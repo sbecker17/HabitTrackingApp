d82fbc27bf60892f99335d8deb76c5d6333c6765
unknown <sarah.becker@drake.edu>
2022-03-07 21:31:56 -0600

    buttons work for most recent task added again -s

--- SpartanGrid.py
+++ SpartanGrid.py
@@ -38,10 +38,12 @@ class SpartanGrid(GridLayout):
         # allHabits = []
         allHabits = get_all_habits(self.connection, 'elena')
         print(allHabits)
-        # self.allHabitsdict = {}
+        self.allHabitsdict = {}
 
         for i in allHabits:
-            
+            # user, cat, name, cnt, date
+            self.allHabitsdict.update({i[2] : [i[0], i[1], i[3], i[4]]})
+            print(self.allHabitsdict)
             # allHabitsdict.update({i[0], i[1],i[2],i[3],i[4]})
             # print(allHabitsdict[0])
             h1 = Habit(i[0],i[1],i[2],i[3],i[4])
@@ -56,6 +58,23 @@ class SpartanGrid(GridLayout):
             self.add_widget(self.habit1cnt)
             self.add_widget(self.didIt)
             self.add_widget(self.didnt)
+
+
+            # allHabitsdict.update({i[0], i[1],i[2],i[3],i[4]})
+            # print(allHabitsdict[0])
+            # h1 = Habit(i[0],i[1],i[2],i[3],i[4])
+            
+            # self.habit1cnt = Label(text = str(h1.count))
+            # self.habit1 = Label(text = h1.name, bold = True)
+            # self.didIt = Button(text = "Did it!", on_press=lambda y:self.count_up(xconnection = self.connection, hab=h1), background_color = [169/255,255/255,221/255,1])
+            # self.didnt = Button(text = "Not today", on_press=lambda z:self.count_down(xconnection=self.connection, hab=h1), background_color = [253/255, 129/255, 129/255, 1])
+            # # self.didnt = Button(text = "Not today",  background_color = [253/255, 129/255, 129/255, 1])
+
+            # self.add_widget(self.habit1)
+            # self.add_widget(self.habit1cnt)
+            # self.add_widget(self.didIt)
+            # self.add_widget(self.didnt)
+                        
 
     def count_up(self, xconnection, hab):
         if (self.habit1.text == str(hab.name)):
