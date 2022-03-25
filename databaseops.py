c24193a11624027ed6624908f1ac8e0422eedc56
unknown <sarah.becker@drake.edu>
2022-03-25 12:13:32 -0500

    fixed buttonsgit add .! in newlayoutcopy.py -s

--- databaseops.py
+++ databaseops.py
@@ -1,6 +1,9 @@
 import sqlite3
 from sqlite3 import Error
 
+import sqlite3
+from sqlite3 import Error
+
 def create_connection(db_file):
     """ create a database connection to a SQLite database"""
     conn = None
@@ -11,9 +14,9 @@ def create_connection(db_file):
     except Error as e:
         print(e)
 
+
 def close_connection(conn):
     conn.close()
-
 # c = conn.cursor()
 def create_habit_table(conn):
     conn.cursor().execute("""CREATE TABLE if not exists habitlist
@@ -26,7 +29,6 @@ def create_habit_table(conn):
                             )"""
                          )
     conn.commit()
-# CREATE TABLE if not exists habitlist
 
 def insert_habit(hab, conn):
     c = conn.cursor()
@@ -34,12 +36,6 @@ def insert_habit(hab, conn):
         c.execute("INSERT INTO habitlist(username, category, name, count, start_date) VALUES (:username, :category, :name, :count, :start_date)",
         {'username': hab.username, 'category': hab.category, 'name': hab.name, 'count':hab.count, 'start_date':hab.start_date})
     conn.commit()
-    #print("1")
-    #print(hab.category)
-    #print(hab.name)
-    #print("2")
-    #print(hab.count)
-    #print("3")
 
 # def insert_habit(conn, hab):
 #     conn.cursor.execute("""INSERT INTO habitlist(category, name, count) VALUES (:category, :name, :count)""", 
@@ -50,31 +46,107 @@ def insert_habit(hab, conn):
 #     #print(hab.name)
 #     #print("2")
 #     #print(hab.count)
-#     #print("3")
-
-# def get_all_habits(conn, username):
-#     conn.cursor().execute("""SELECT * FROM habitlist where count = 0""")
-#     return conn.cursor().fetchall()
+#     #print("3s")
 
 def get_all_habits(conn, username):
-    conn.cursor().execute("SELECT * FROM habitlist WHERE username= :username", {'username': username})
-    return conn.cursor().fetchall()
+    c = conn.cursor()
+    c.execute("SELECT * FROM habitlist WHERE username =:username", {'username': username})
+    # typ = type(conn.cursor().fetchall())
+    # print(typ)
+    return c.fetchall()
+
 
 def get_first_habit(conn):
-    conn.cursor().execute("SELECT category FROM habitlist LIMIT 1")
+    conn.cursor().execute("SELECT * FROM habitlist LIMIT 1")
     # print(conn.cursor().fetchall())
     # return conn.cursor().fetchall()
-    data = conn.cursor().fetchall()
-    print(data)
-    for row in data:
-        print(row)
-        print(type(conn.cursor().fetchall()))
+    return conn.cursor().fetchall()
+    
 
 def update_count(count, name, conn):
     conn.cursor().execute("UPDATE habitlist SET count = :count WHERE name = :name", {'count': count, 'name': name})
     conn.commit()
 
-def check_yes(habit, conn):
-    count = habit.count+1
-    conn.cursor().execute("UPDATE habitlist SET count = :count WHERE name = :name", {'count': count, 'name': habit.name})
-    conn.commit()
+# def check_yes(habit, conn):
+#     conn.cursor().execute("UPDATE habitlist SET count = :count WHERE name = :name", {'count': habit.count + 1, 'name': habit.name})
+#     conn.commit()
+    # here, I want to add one to the count of a habit
+
+
+# def create_connection(db_file):
+#     """ create a database connection to a SQLite database"""
+#     conn = None
+#     try:
+#         conn = sqlite3.connect(db_file)
+#         print(sqlite3.version)
+#         return conn
+#     except Error as e:
+#         print(e)
+
+# def close_connection(conn):
+#     conn.close()
+
+# # c = conn.cursor()
+# def create_habit_table(conn):
+#     conn.cursor().execute("""CREATE TABLE if not exists habitlist
+#                             (
+#                             username text,
+#                             category text,
+#                             name text,
+#                             count integer,
+#                             start_date date
+#                             )"""
+#                          )
+#     conn.commit()
+# # CREATE TABLE if not exists habitlist
+
+# def insert_habit(hab, conn):
+#     c = conn.cursor()
+#     with conn:
+#         c.execute("INSERT INTO habitlist(username, category, name, count, start_date) VALUES (:username, :category, :name, :count, :start_date)",
+#         {'username': hab.username, 'category': hab.category, 'name': hab.name, 'count':hab.count, 'start_date':hab.start_date})
+#     conn.commit()
+#     #print("1")
+#     #print(hab.category)
+#     #print(hab.name)
+#     #print("2")
+#     #print(hab.count)
+#     #print("3")
+
+# # def insert_habit(conn, hab):
+# #     conn.cursor.execute("""INSERT INTO habitlist(category, name, count) VALUES (:category, :name, :count)""", 
+# #         {'category': hab.category, 'name': hab.name, 'count':hab.count})
+# #     conn.commit()
+# #     #print("1")
+# #     #print(hab.category)
+# #     #print(hab.name)
+# #     #print("2")
+# #     #print(hab.count)
+# #     #print("3")
+
+# # def get_all_habits(conn, username):
+# #     conn.cursor().execute("""SELECT * FROM habitlist where count = 0""")
+# #     return conn.cursor().fetchall()
+
+# def get_all_habits(conn, username):
+#     conn.cursor().execute("SELECT * FROM habitlist WHERE username= :username", {'username': username})
+#     return conn.cursor().fetchall()
+
+# def get_first_habit(conn):
+#     conn.cursor().execute("SELECT category FROM habitlist LIMIT 1")
+#     # print(conn.cursor().fetchall())
+#     # return conn.cursor().fetchall()
+#     data = conn.cursor().fetchall()
+#     print(data)
+#     for row in data:
+#         print(row)
+#         print(type(conn.cursor().fetchall()))
+
+# def update_count(count, name, conn):
+#     conn.cursor().execute("UPDATE habitlist SET count = :count WHERE name = :name", {'count': count, 'name': name})
+#     conn.commit()
+
+# def check_yes(habit, conn):
+#     count = habit.count+1
+#     conn.cursor().execute("UPDATE habitlist SET count = :count WHERE name = :name", {'count': count, 'name': habit.name})
+#     conn.commit()
--- newlayoutcopy.py
+++ newlayoutcopy.py
@@ -1,36 +1,17 @@
-import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
-from kivy.uix.widget import Widget
-from kivy.lang import Builder
-import kivy.properties as kyprops
-from kivy.uix.scrollview import ScrollView
 from datetime import date
-import weakref
-from kivy.clock import Clock
-#from kivymd.app import MDApp
-#from kivy.properties import ObjectProperty
-
-import os
-
-import sqlite3
-from sqlite3 import Error
-
-import os
-
-# i can't figure out how to import all the functions at once -- elena
-from databaseops import create_connection, insert_habit, close_connection, create_habit_table, get_all_habits, get_first_habit
 from habit import Habit
+from databaseops import *
 import sys
+from functools import partial 
 
 
-class SpartanGrid(FloatLayout):
+class SpartanGrid(GridLayout):
 
     def __init__(self, **kwargs):
         db_name = "elena.db"
@@ -58,16 +39,16 @@ class SpartanGrid(FloatLayout):
 
         # allHabits = []
         allHabits = get_all_habits(self.connection, 'elena')
-        # print(allHabits)
-        # self.allHabitsdict = {}
+        print(allHabits)
+        self.allHabitsdict = {}
 
-        # for i in allHabits:
-        #     # user, cat, name, cnt, date
-        #     # name : [user, cat, cnt, date]
-        #     self.allHabitsdict.update({i[2] : [i[0], i[1], i[3], i[4]]})
-        #     print(self.allHabitsdict)
-        #     # allHabitsdict.update({i[0], i[1],i[2],i[3],i[4]})
-        #     # print(allHabitsdict[0])
+        for i in allHabits:
+            # user, cat, name, cnt, date
+            # name : [user, cat, cnt, date]
+            self.allHabitsdict.update({i[2] : [i[0], i[1], i[3], i[4]]})
+            # print(self.allHabitsdict)
+            # allHabitsdict.update({i[0], i[1],i[2],i[3],i[4]})
+            # print(self.allHabitsdict[0])
 
         # for key,value in self.allHabitsdict.items():
         #     self.key = Label(text = str(key), bold = True)
@@ -80,36 +61,133 @@ class SpartanGrid(FloatLayout):
         #     self.didnt = Button(text = "Not today", on_press=lambda z:self.count_down(xconnection=self.connection, hab=h1), background_color = [253/255, 129/255, 129/255, 1])
         #     # self.didnt = Button(text = "Not today",  background_color = [253/255, 129/255, 129/255, 1])
 
+        # for key, value in self.allHabitsdict.items():
+
+
         #     self.add_widget(self.key)
         #     self.add_widget(self.keycount)
         #     self.add_widget(self.didIt)
         #     self.add_widget(self.didnt)
 
-        for i in allHabits:
-            # allHabitsdict.update({i[0], i[1],i[2],i[3],i[4]})
-            # print(allHabitsdict[0])
+        self.h0 = Label()
+        self.h1 = Label()
+        self.h2 = Label()
+        self.h3 = Label()
+        self.h4 = Label()
+        self.h5 = Label()
+        self.h6 = Label()
+        self.h7 = Label()
+        self.h8 = Label()
+        self.h9 = Label()
+        self.h10 = Label()
+        self.counth0 = Label()
+        self.counth1 = Label()
+        self.counth2 = Label()
+        self.counth3 = Label()
+        self.counth4 = Label()
+        self.counth5 = Label()
+        self.counth6 = Label()
+        self.counth7 = Label()
+        self.counth8 = Label()
+        self.counth9 = Label()
+        self.counth10 = Label()
+        self.didIt0 = Button()
+        self.didIt1 = Button()
+        self.didIt2 = Button()
+        self.didIt3 = Button()
+        self.didIt4 = Button()
+        self.didIt5 = Button()
+        self.didIt6 = Button()
+        self.didIt7 = Button()
+        self.didIt8 = Button()
+        self.didIt9 = Button()
+        self.didIt10 = Button()
+        self.didnt0 = Button()
+        self.didnt1 = Button()
+        self.didnt2 = Button()
+        self.didnt3 = Button()
+        self.didnt4 = Button()
+        self.didnt5 = Button()
+        self.didnt6 = Button()
+        self.didnt7 = Button()
+        self.didnt8 = Button()
+        self.didnt9 = Button()
+        self.didnt10 = Button()
+        self.habit_name_labels = [self.h0, self.h1, self.h2, self.h3, self.h4, self.h5, self.h6, self.h7, self.h8, self.h9, self.h10]
+        self.habit_count_labels = [self.counth0, self.counth1, self.counth2, self.counth3, self.counth4, self.counth5, self.counth6, self.counth7, self.counth8, self.counth9, self.counth10]
+        self.check_yes_buttons = [self.didIt0, self.didIt1, self.didIt2, self.didIt3, self.didIt4, self.didIt5, self.didIt6, self.didIt7, self.didIt8, self.didIt9, self.didIt10]
+        #                     self.didIt11, self.didIt12, self.didIt13, self.didIt14, self.didIt15, self.didIt16, self.didIt17, self.didIt18, self.didIt19]
+        self.check_no_buttons = [self.didnt0, self.didnt1, self.didnt2, self.didnt3, self.didnt4, self.didnt5, self.didnt6, self.didnt7, self.didnt8, self.didnt9, self.didnt10]
+        #                     self.didnt11, self.didnt12, self.didnt13, self.didnt14, self.didnt15, self.didnt16, self.didnt17, self.didnt18, self.didnt19]
+        
+        for i in range(len(allHabits)):
+        # for i in range(2):
+            print(i)
+            # print("Habit cnt: ", self.habit_count_labels[i])
+            self.habit_count_labels[i].text = str(allHabits[i][3])
+            # self.habit_count_labels[i].ids = {str(allHabits[i][2])+"_count": self.habit_count_labels[i]}
+            self.habit_name_labels[i].text = str(allHabits[i][2])
+            # self.habit_name_labels[i].ids = {str(allHabits[i][2]): self.habit_name_labels[i]}
+            # print(self.habit_name_labels[i].ids)
+            # self.ids.update(self.habit_name_labels[i].ids) 
+            # print(self.ids)
+            # self.ids.update(self.habit_count_labels[i].ids) 
+            # print(self.ids)
+            # cntLabel = self.ids[str(allHabits[i][2])+"_count"]
+            # print("hbt cnt label: ", cntLabel)
+            # print(self.ids[str(allHabits[i][2])])
+            self.habit_count_labels[i] = Label(text = str(allHabits[i][3]))
+            # print(self.check_yes_buttons[i])
+            self.check_yes_buttons[i] = Button(text = "Did it!", on_press = partial(self.count_up_new, self.connection, str(allHabits[i][2]), i), background_color = [169/255,255/255,221/255,1])
+            # self.check_yes_buttons[i] = Button(text = "Did it!", on_press = lambda x:self.count_up_new(xconnection = self.connection, loc = i), background_color = [169/255,255/255,221/255,1])
+            # self.check_yes_buttons[i].ids= {str(allHabits[i][2])+"_yes"+str(i): self.check_yes_buttons[i]}
+            # self.ids.update(self.check_yes_buttons[i].ids) 
+            # print(self.ids)
+            self.check_no_buttons[i] = Button(text = "Didn't", on_press = partial(self.count_down_new, self.connection, i), background_color = [253/255, 129/255, 129/255, 1])
+            self.add_widget(self.habit_name_labels[i])
+            self.add_widget(self.habit_count_labels[i])
+            self.add_widget(self.check_yes_buttons[i])
+            # print("yes btn: ", self.check_yes_buttons[i])
+            self.add_widget(self.check_no_buttons[i])
 
-            h1 = Habit(i[0],i[1],i[2],i[3],i[4])
+
+        # for i in allHabits:
+        #     # allHabitsdict.update({i[0], i[1],i[2],i[3],i[4]})
+        #     # print(allHabitsdict[0])
+        #     print(i)
+        #     h1 = Habit(i[0],i[1],i[2],i[3],i[4])
             
-            self.habit1cnt = Label(text = str(h1.count))
-            self.habit1 = Label(text = h1.name, bold = True)
-            self.didIt = Button(text = "Did it!", on_press=lambda y:self.count_up(xconnection = self.connection, hab=h1), background_color = [169/255,255/255,221/255,1])
-            self.didnt = Button(text = "Not today", on_press=lambda z:self.count_down(xconnection=self.connection, hab=h1), background_color = [253/255, 129/255, 129/255, 1])
-            # self.didnt = Button(text = "Not today",  background_color = [253/255, 129/255, 129/255, 1])
+        #     self.habit1cnt = Label(text = str(h1.count))
+        #     self.habit1 = Label(text = h1.name, bold = True)
+        #     self.didIt = Button(text = "Did it!", on_press=lambda y:self.count_up_dict(xconnection = self.connection, hab_key=i[2]), background_color = [169/255,255/255,221/255,1])
+        #     self.didnt = Button(text = "Not today", on_press=lambda z:self.count_down(xconnection=self.connection, hab=h1), background_color = [253/255, 129/255, 129/255, 1])
+        #     # self.didnt = Button(text = "Not today",  background_color = [253/255, 129/255, 129/255, 1])
 
-            self.add_widget(self.habit1)
-            self.add_widget(self.habit1cnt)
-            self.add_widget(self.didIt)
-            self.add_widget(self.didnt)
+        #     self.add_widget(self.habit1)
+        #     self.add_widget(self.habit1cnt)
+        #     self.add_widget(self.didIt)
+        #     self.add_widget(self.didnt)
                         
 
     def count_up(self, xconnection, hab):
+        print(self.habit1.text)
+        print(str(hab.name))
         if (self.habit1.text == str(hab.name)):
             self.habit1cnt.text = str(int(hab.count)+1)
             hab.count = hab.count+1
             update_count(self.habit1cnt.text, hab.name, xconnection)
         else:
             pass
+
+    def count_up_new(self, xconnection, loc, ind, instance):
+        name = self.habit_name_labels[ind].text
+        # cntUpdate = self.ids[loc+"_count"]
+        self.habit_count_labels[ind].text = str(int(self.habit_count_labels[ind].text) + 1)
+        # self.ids[loc+"_count"].text = str(int(cntUpdate.text) + 1)
+        update_count(self.habit_count_labels[ind].text, name, xconnection)
+        pass
+
+
 
     def count_down(self, xconnection, hab):
         if (self.habit1.text == str(hab.name)):
@@ -119,6 +197,12 @@ class SpartanGrid(FloatLayout):
             update_count(0, self.habit1.text, xconnection)
         else: 
             pass
+
+    def count_down_new(self, xconnection, ind, instance):
+        name = self.habit_name_labels[ind].text
+        self.habit_count_labels[ind].text = str(0)
+        update_count(0, name, xconnection)
+        pass
         
     def click_me(self, xconnection, db_name):
         h1 = Habit(db_name[:-3], self.t_cat.text, self.t_name.text, 0, date.today())
@@ -156,3 +240,16 @@ class SpartanGrid(FloatLayout):
         insert_habit(h2, xconnection)
         get_first_habit(xconnection)
         self.popup.dismiss()
+
+class SpartanApp(App):
+
+    def build(self):
+        return SpartanGrid()
+
+if __name__  == "__main__":
+    # This line is where we would specify which database the user connects to based on their user information (maybe just a username?)
+    # If a matching db already exists, it will connect to that one, if not it creates a new one.
+    # create_habit_table(xconnection)
+    SpartanApp().run()
+
+    close_connection(SpartanGrid.connection)
