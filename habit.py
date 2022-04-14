f97b24e5ef5ad9d02256d6658b3e2f1c1638e793
anniewadsworth <99425085+anniewadsworth@users.noreply.github.com>
2022-04-14 14:18:42 -0500

    Add files via upload

--- habit.py
+++ habit.py
@@ -1,10 +1,17 @@
+from datetime import date
+
 
 class Habit:
 
-    def __init__(self, category, name):
+    def __init__(self, username, category, name, count, start_date, last_modified_date, max_quit_count, habit_occurrence):
+        self.username = username
         self.category = category
         self.name = name
+        self.count = count
+        self.start_date = start_date
+        self.last_modified_date = last_modified_date
+        self.max_quit_count = max_quit_count
+        self.habit_occurrence = habit_occurrence
 
     def __repr__(self):
-        return "Habit('{}','{}',{})".format(self.category, self.name)
-        
+        return "Habit('{}','{}','{}','{}','{}','{}','{}', '{}', {})".format(self.username, self.category, self.name, self.count, self.start_date, self.last_modified_date, self.max_quit_count, self.habit_occurrence)
